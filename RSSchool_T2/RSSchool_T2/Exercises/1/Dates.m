516324213b54fbcb2ce388dba34b5c44e8157171
Dmitry Marchenko <dmmarychev@yandex.ru>
2020-03-19 23:33:12 +0300

    feat: add solution for Test1

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,27 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    
+    NSCalendar *calendar = [NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian];
+    
+    NSDateComponents *components = [[NSDateComponents alloc] init];
+    components.year = [year integerValue];
+    components.month = [month integerValue];
+    components.day = [day integerValue];
+    
+    if ([components isValidDateInCalendar:calendar]) {
+        
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+        dateFormatter.dateFormat = @"dd MMMM, EEEE";
+        
+        NSDate *currentDate = [calendar dateFromComponents:components];
+        
+        return [dateFormatter stringFromDate:currentDate];
+    } else {
+        return @"Такого дня не существует";
+    }
+
 }
 
 @end
