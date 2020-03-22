91eba217cbf395107348d52f5871a989206420e4
Dmitry Marchenko <dmmarychev@yandex.ru>
2020-03-22 16:30:05 +0300

    feat: added solution for test 5-6

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -1,27 +1,75 @@
 #import "Blocks.h"
 
+@interface Blocks ()
+
+@property (strong, nonatomic) __block NSArray *array;
+
+@end
+
 @implementation Blocks
 
-- (instancetype)init
-{
+#pragma mark - Init & dealloc
+
+- (instancetype)init {
     self = [super init];
     if (self) {
-        
-        _blockA = ^(NSArray *array) {
-            
-        };
-        
-        _blockB = ^(Class class) {
-            
-        };
-        
-        _blockC = ^(NSObject *object) {
-            
-        };
-        
+        [self configurateBlocks];
     }
     return self;
 }
 
-@end
 
+#pragma mark - Blocks
+
+- (void)configurateBlocks {
+    
+    self.blockA = ^(NSArray *array) {
+        self.array = array;
+    };
+    
+    self.blockB = ^(Class class) {
+        
+        NSMutableArray *objectsOfCurrentClass = [NSMutableArray array];
+        
+        for (NSObject *currentObject in self.array) {
+            
+            if ([currentObject isKindOfClass:class] && currentObject != NULL && currentObject != nil) {
+                [objectsOfCurrentClass addObject:currentObject];
+            }
+        }
+        
+        if ([objectsOfCurrentClass count] > 1) {
+            
+            if (class == [NSString class]) {
+                
+                self.blockC([objectsOfCurrentClass componentsJoinedByString:@""]);
+                
+            } else if (class == [NSNumber class]) {
+                
+                int sum = 0;
+                
+                for (NSNumber *currentNumber in objectsOfCurrentClass) {
+                    sum += [currentNumber intValue];
+                }
+                
+                self.blockC([NSNumber numberWithInt:sum]);
+                
+            } else if (class == [NSDate class]){
+                
+                NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+                formatter.dateFormat = @"dd.MM.yyyy";
+                
+                NSDate *resultDate = [objectsOfCurrentClass firstObject];
+                
+                for (NSDate *currentDate in objectsOfCurrentClass) {
+                    
+                    resultDate = [currentDate laterDate:resultDate];
+                }
+                
+                self.blockC([formatter stringFromDate:resultDate]);
+            }
+        }
+    };
+}
+
+@end
--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -1,7 +1,30 @@
 #import "FibonacciResolver.h"
 
 @implementation FibonacciResolver
+
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
+    
+    int intValueOfNumber = [number intValue];
+    
+    NSMutableArray *fibonacciNumbers = [NSMutableArray arrayWithObjects:@0, @1, nil];
+    
+    while (YES) {
+
+        NSNumber *penultimateFibonacciNumber = fibonacciNumbers[[fibonacciNumbers count] - 2];
+        NSNumber *lastFibonacciNumber = [fibonacciNumbers lastObject];
+        
+        int productOfNumbers = [lastFibonacciNumber intValue] * [penultimateFibonacciNumber intValue];
+        int sumOfNumbers = [lastFibonacciNumber intValue] + [penultimateFibonacciNumber intValue];
+        
+        if (productOfNumbers < intValueOfNumber) {
+            
+            [fibonacciNumbers addObject:[NSNumber numberWithInt:sumOfNumbers]];
+            
+        } else {
+            return @[penultimateFibonacciNumber, lastFibonacciNumber, intValueOfNumber == productOfNumbers ? @1 : @0];
+        }
+    }
     return @[];
 }
+
 @end
